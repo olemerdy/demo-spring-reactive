255e22af4ca5a973a0d0068a60d516ddcba33d91
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2023-12-18 14:18:27 +0100

    Fixing the Elasticsearch issue

--- elasticsearch/src/main/kotlin/org/lafeuille/demo/elasticsearch/data/ElasticsearchEntity.kt
+++ elasticsearch/src/main/kotlin/org/lafeuille/demo/elasticsearch/data/ElasticsearchEntity.kt
@@ -4,9 +4,11 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.annotation.Version
+import org.springframework.data.elasticsearch.annotations.Document
 import java.time.Instant
 import java.util.*
 
+@Document(indexName = "entities")
 data class ElasticsearchEntity(
     @Id val id: UUID = UUID.randomUUID(),
     @CreatedDate val createdDate: Instant? = null,
