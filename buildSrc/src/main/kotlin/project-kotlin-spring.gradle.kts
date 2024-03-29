c70cdc0403bb22544303cff04256f58461e45500
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2024-03-29 16:09:10 +0100

    Upgrade Spring Cloud

--- buildSrc/src/main/kotlin/project-kotlin-spring.gradle.kts
+++ buildSrc/src/main/kotlin/project-kotlin-spring.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
     api(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
-    api(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.0"))
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.1"))
 
     constraints {
         testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
