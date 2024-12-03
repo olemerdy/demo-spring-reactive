bef802b38a8991e2f2fbf025c392d1f37f9b5460
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2024-12-03 10:33:38 +0100

    Dependencies

--- buildSrc/src/main/kotlin/project-kotlin-spring.gradle.kts
+++ buildSrc/src/main/kotlin/project-kotlin-spring.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 
 dependencies {
     api(platform("org.springframework.boot:spring-boot-dependencies:3.4.0"))
-    api(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.3"))
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:2024.0.0"))
 
     constraints {
+        testImplementation("org.apache.commons:commons-compress:[1.26+,)") // Security fix for TestContainers 1.20.4
         testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
         testImplementation(kotlin("test"))
     }
