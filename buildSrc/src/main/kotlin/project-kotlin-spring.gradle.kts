1c9b3762bcd8d6efea9dee85fcf0b85139b192a7
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-05-29 23:31:45 +0200

    More Spring Dependency Management

--- buildSrc/src/main/kotlin/project-kotlin-spring.gradle.kts
+++ buildSrc/src/main/kotlin/project-kotlin-spring.gradle.kts
@@ -13,11 +13,13 @@ dependencyManagement {
         mavenBom("org.springframework.boot:spring-boot-dependencies:${libs.versions.spring.boot.get()}")
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.spring.cloud.get()}")
     }
+    dependencies {
+        dependency("org.mockito.kotlin:mockito-kotlin:${libs.versions.mockito.kotlin.get()}")
+    }
 }
 
 dependencies {
     constraints {
-        testImplementation(libs.mockito.kotlin)
         testImplementation(kotlin("test"))
     }
 }
