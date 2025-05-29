d3b73a7fff1003aaef5e65026147f1a12fdfa2f7
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-05-29 22:32:34 +0200

    Disable TLS on Elasticsearch for now

--- contents-elasticsearch/src/test/kotlin/org/lafeuille/demo/infra/elasticsearch/ElasticsearchContainerTestConfiguration.kt
+++ contents-elasticsearch/src/test/kotlin/org/lafeuille/demo/infra/elasticsearch/ElasticsearchContainerTestConfiguration.kt
@@ -2,7 +2,6 @@ package org.lafeuille.demo.infra.elasticsearch
 
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.springframework.boot.testcontainers.service.connection.Ssl
 import org.springframework.context.annotation.Bean
 import org.testcontainers.elasticsearch.ElasticsearchContainer
 
@@ -10,6 +9,7 @@ import org.testcontainers.elasticsearch.ElasticsearchContainer
 class ElasticsearchContainerTestConfiguration {
     @Bean
     @ServiceConnection
-    @Ssl
-    fun elasticsearch() = ElasticsearchContainer(ElasticsearchDefaults.DOCKER_IMAGE_NAME)
+    fun elasticsearch(): ElasticsearchContainer =
+        ElasticsearchContainer(ElasticsearchDefaults.DOCKER_IMAGE_NAME)
+            .withEnv("xpack.security.enabled", "" + false)
 }
