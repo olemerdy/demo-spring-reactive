550d3e1b1aeb3de0d707746e1925661d3feb3942
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-03-21 14:25:25 +0100

    Configuring R2DBC

--- infra-r2dbc/build.gradle.kts
+++ infra-r2dbc/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 dependencies {
     implementation(project(":infra-shared"))
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    testFixturesApi("org.testcontainers:testcontainers-r2dbc")
 }
