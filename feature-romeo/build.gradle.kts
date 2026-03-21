9bb8db6e59b0593847258404f43ba8bb8f8e4471
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-03-21 17:06:30 +0100

    Fixing Kotlin Jackson tooling

--- feature-romeo/build.gradle.kts
+++ feature-romeo/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("tools.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
--- feature-sierra/build.gradle.kts
+++ feature-sierra/build.gradle.kts
@@ -7,14 +7,16 @@ dependencies {
     implementation(project(":infra-shared"))
     implementation(project(":infra-cassandra"))
 
+    implementation("org.springframework.boot:spring-boot-starter-data-cassandra")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("tools.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     testImplementation(testFixtures(project(":infra-cassandra")))
 
+    testImplementation("org.springframework.boot:spring-boot-starter-data-cassandra-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.mockito.kotlin:mockito-kotlin")
 }
