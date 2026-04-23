b4ffd58eede9808914832b4e8c227ec8d238823e
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-04-23 22:43:45 +0200

    More Charlie

--- feature-charlie/src/test/kotlin/org/lafeuille/demo/charlie/data/CharlieRepositoryTest.kt
+++ feature-charlie/src/test/kotlin/org/lafeuille/demo/charlie/data/CharlieRepositoryTest.kt
@@ -2,12 +2,31 @@ package org.lafeuille.demo.charlie.data
 
 import org.junit.jupiter.api.Test
 import org.lafeuille.demo.infra.couchbase.CouchbaseContainerTestConfiguration
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.data.couchbase.test.autoconfigure.DataCouchbaseTest
 import org.springframework.context.annotation.Import
+import reactor.kotlin.test.test
 
 @DataCouchbaseTest
 @Import(CouchbaseContainerTestConfiguration::class)
-class CharlieRepositoryTest {
+class CharlieRepositoryTest(
+    @Autowired val repository: CharlieRepository,
+) {
     @Test
-    fun test() {}
+    fun test_findAll() {
+        repository
+            .findAll()
+            .test()
+            .verifyComplete()
+    }
+
+    @Test
+    fun test_save() {
+        val charlie = CharlieFixtures.defaultCharlie()
+        repository
+            .save(charlie)
+            .test()
+            .expectNext(charlie)
+            .verifyComplete()
+    }
 }
